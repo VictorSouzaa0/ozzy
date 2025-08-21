85ea10c301a30af4e2fbb33bbefcaa5e6d69eb68
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-08-21 17:47:23 -0300

    .

--- src/pages/Discography.jsx
+++ src/pages/Discography.jsx
@@ -3,7 +3,8 @@ import styles from "../modules/Discography.module.css"
 export default function Discography() {
     return(
         <main>
-            
+           <>
+           </> 
         </main>
     )
 }
